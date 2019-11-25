481d53f99fa35719e807ec69623765e9d62e44e9
Mahadi Sajjad Neloy <sierra117@qq.com>
2019-11-25 14:01:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -5,14 +5,8 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const session = require('express-session');
 const app = express();
-
-// Passport Config
 require('./config/passport')(passport);
-
-// DB Config
 const db = require('./config/keys').mongoURI;
-
-// Connect to MongoDB
 mongoose
   .connect(
     db, {
@@ -21,17 +15,11 @@ mongoose
   )
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
-
-// EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
-
-// Express body parser
 app.use(express.urlencoded({
   extended: true
 }));
-
-// Express session
 app.use(
   session({
     secret: 'secret',
@@ -39,22 +27,15 @@ app.use(
     saveUninitialized: true
   })
 );
-
-// Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
-
-// Connect flash
 app.use(flash());
-// Global variables
 app.use(function (req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
   next();
 });
-
-// Routes
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
